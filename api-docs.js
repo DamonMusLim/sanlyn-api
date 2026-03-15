2f909fc20abb7cf50c84247b88b815ceb6b9afd9
Damon <168725@gmail.com>
2026-03-16 01:28:56 +0800

    fix: use module.exports instead of export default

--- api-docs.js
+++ api-docs.js
@@ -13,7 +13,7 @@ const DOC_MAP = {
   co:          '_widget_1763604147210',
 };
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
